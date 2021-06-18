b898924db8ce82f62dc130df2e73f0e13b911388
Javier Ale <javierale25@live.com>
2021-06-18 10:59:50 -0300

    Creating routes tasks

--- api/src/routes/tasks.js
+++ api/src/routes/tasks.js
@@ -14,5 +14,68 @@ router.get('/', (req, res) => {
     });
 });
 
+// GET by ID
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('SELECT * FROM tasks WHERE id = ?', [id], (err, rows, fields) => {
+        if (!err) {
+            res.json(rows[0]);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+// DELETE Task
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('DELETE FROM tasks WHERE id = ?', [id], (err, rows, fields) => {
+        if (!err) {
+            res.json({ status: 'Task Deleted' });
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+// INSERT Task
+router.post('/', (req, res) => {
+    const { id, description, completed, folderID } = req.body;
+    console.log(id, description, completed, folderID);
+    const query = `
+    SET @id = ?;
+    SET @description = ?;
+    SET @completed = ?;
+    SET @folderID = ?;
+    CALL TaskIU(@id, @description, @completed, @folderID);
+  `;
+    mysqlConnection.query(query, [id, description, completed, folderID], (err, rows, fields) => {
+        if (!err) {
+            res.json({ status: 'Task Saved' });
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+// UPDATE Task
+router.put('/:id', (req, res) => {
+    const { description, completed, folderID } = req.body;
+    const { id } = req.params;
+    const query = `
+    SET @id = ?;
+    SET @description = ?;
+    SET @completed = ?;
+    SET @folderID = ?;
+    CALL TaskIU(@id, @description, @completed, @folderID);
+    `;
+    mysqlConnection.query(query, [id, description, completed, folderID], (err, rows, fields) => {
+        if (!err) {
+            res.json({ status: 'Task Updated' });
+        } else {
+            console.log(err);
+        }
+    });
+});
 
 module.exports = router;
