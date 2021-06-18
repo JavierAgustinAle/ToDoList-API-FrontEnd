330674a7c238e6f89bdbeb160005ee8d9cbe8c48
Javier Ale <javierale25@live.com>
2021-06-17 21:10:51 -0300

    Commit initial

---
+++ api/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.json());
+
+// Routes
+app.use(require('./routes/tasks'));
+
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
