ee8232deaa4e30b8f31d96fbb575640cb63c4ed7
Javier Ale <javierale25@live.com>
2021-06-19 20:28:38 -0300

    Button Back added to folders

--- frontend/frontToDoList/src/app/Components/folders/folders.component.ts
+++ frontend/frontToDoList/src/app/Components/folders/folders.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { FolderService } from '../../services/folder.service';
 import { IFolder } from '../../models/folder.model';
 import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { SubSink } from 'SubSink';
 
 @Component({
   selector: 'app-folders',
   templateUrl: './folders.component.html',
   styleUrls: ['./folders.component.css']
 })
-export class FoldersComponent implements OnInit {
+export class FoldersComponent implements OnInit, OnDestroy {
+  private subs = new SubSink();
   foldersAll: IFolder[];
   show = false;
   folderSelected: IFolder;
@@ -21,40 +24,55 @@ export class FoldersComponent implements OnInit {
   }
 
   loadTable(): void {
-    this.folderService.getAllFolders().subscribe((res: IFolder[]) => {
-      this.foldersAll = res;
-    })
+    this.subs.add(
+      this.folderService.getAllFolders().subscribe((res: IFolder[]) => {
+        this.foldersAll = res;
+      })
+    )
   }
 
-  deleteFolder(folder) {
-    this.folderService.deleteFolder(folder.id).subscribe(_ => {
-      this.openSnackBar('Folder Deleted');
-      this.loadTable();
-    })
+  deleteFolder(folder): void {
+    this.subs.add(
+      this.folderService.deleteFolder(folder.id).subscribe(_ => {
+        this.openSnackBar('Folder Deleted');
+        this.loadTable();
+      })
+    )
   }
 
-  openSnackBar(message: string) {
+  openSnackBar(message: string): void {
     this._snackBar.open(message, 'X', {
       duration: 3000
     });
   }
 
-  onSubmit() {
+  onSubmit(): void {
     if ((document.getElementById('addFolder') as HTMLFormElement).value != '') {
       const obj: IFolder = {
         id: 0,
         name: (document.getElementById('addFolder') as HTMLFormElement).value
       };
-      this.folderService.putFolder(obj).subscribe(_ => {
-        this.openSnackBar('Folder Created');
-        this.loadTable();
-        (document.getElementById('addFolder') as HTMLFormElement).value = '';
-      })
+      this.subs.add(
+        this.folderService.putFolder(obj).subscribe(_ => {
+          this.openSnackBar('Folder Created');
+          this.loadTable();
+          (document.getElementById('addFolder') as HTMLFormElement).value = '';
+        })
+      )
     }
   }
 
-  viewItems(folder) {
+  viewItems(folder): void {
     this.show = !this.show;
     this.folderSelected = folder;
+  }
+
+  back() {
+    this.show = !this.show;
+    this.folderSelected = null;
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
   }
 }
--- frontend/frontToDoList/src/app/Components/tasks/tasks.component.ts
+++ frontend/frontToDoList/src/app/Components/tasks/tasks.component.ts
@@ -1,14 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { TaskService } from 'src/app/services/task.service';
 import { ITask } from '../../models/task.model';
+
+import { SubSink } from 'SubSink';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent implements OnInit, OnDestroy {
+  private subs = new SubSink();
   @Input() folder = null;
   tasksAll: ITask[];
   edit = false;
@@ -20,34 +23,39 @@ export class TasksComponent implements OnInit {
     this.loadTable();
   }
 
-  loadTable() {
+  loadTable(): void {
     if (this.folder != null) {
-      this.taskService.getTaskByFolder(this.folder.id).subscribe((res: ITask[]) => {
-        this.tasksAll = res;
-      })
+      this.subs.add(
+        this.taskService.getTaskByFolder(this.folder.id).subscribe((res: ITask[]) => {
+          this.tasksAll = res;
+        })
+      );
     } else {
-      this.taskService.getAllTasks().subscribe((res: ITask[]) => {
-        this.tasksAll = res;
-      })
+      this.subs.add(
+        this.taskService.getAllTasks().subscribe((res: ITask[]) => {
+          this.tasksAll = res;
+        })
+      )
     }
-
   }
 
 
-  checkComplete(task) {
+  checkComplete(task): void {
     const obj: ITask = {
       id: task.id,
       description: task.description,
       completed: task.completed === 0 ? 1 : 0,
       folderID: task.folderID
     };
-    this.taskService.putTask(obj).subscribe(_ => {
-      this.openSnackBar('Task Update');
-      this.loadTable();
-    })
+    this.subs.add(
+      this.taskService.putTask(obj).subscribe(_ => {
+        this.openSnackBar('Task Update');
+        this.loadTable();
+      })
+    )
   }
 
-  onSubmit() {
+  onSubmit(): void {
     if ((document.getElementById('addTask') as HTMLFormElement).value != '') {
       const obj: ITask = {
         id: 0,
@@ -55,34 +63,38 @@ export class TasksComponent implements OnInit {
         completed: 0,
         folderID: this.folder ? this.folder.id : null
       };
-      this.taskService.putTask(obj).subscribe(_ => {
-        this.openSnackBar('Task Added');
-        this.loadTable();
-        (document.getElementById('addTask') as HTMLFormElement).value = '';
-      })
+      this.subs.add(
+        this.taskService.putTask(obj).subscribe(_ => {
+          this.openSnackBar('Task Added');
+          this.loadTable();
+          (document.getElementById('addTask') as HTMLFormElement).value = '';
+        })
+      )
     }
   }
 
-  delTask(task: ITask) {
-    this.taskService.deleteTask(task.id).subscribe(_ => {
-      this.openSnackBar('Task Deleted');
-      this.loadTable();
-    })
+  delTask(task: ITask): void {
+    this.subs.add(
+      this.taskService.deleteTask(task.id).subscribe(_ => {
+        this.openSnackBar('Task Deleted');
+        this.loadTable();
+      })
+    )
   }
 
 
-  openSnackBar(message: string) {
+  openSnackBar(message: string): void {
     this._snackBar.open(message, 'X', {
       duration: 3000
     });
   }
 
-  editTask(task?) {
+  editTask(task?): void {
     this.edit = !this.edit;
     this.edit ? this.editingTask = task : this.editingTask = null;
   }
 
-  update() {
+  update(): void {
     if ((document.getElementById('updateTask') as HTMLFormElement).value != '') {
       const obj: ITask = {
         id: this.editingTask.id,
@@ -90,12 +102,19 @@ export class TasksComponent implements OnInit {
         completed: this.editingTask.completed,
         folderID: this.editingTask.folderID
       };
-      this.taskService.putTask(obj).subscribe(_ => {
-        this.openSnackBar('Task Updated');
-        this.loadTable();
-        (document.getElementById('updateTask') as HTMLFormElement).value = '';
-        this.editTask();
-      })
+      this.subs.add(
+        this.taskService.putTask(obj).subscribe(_ => {
+          this.openSnackBar('Task Updated');
+          this.loadTable();
+          (document.getElementById('updateTask') as HTMLFormElement).value = '';
+          this.editTask();
+        })
+      )
     }
+  }
+
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
   }
 }
