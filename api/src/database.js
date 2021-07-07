43e1eb632216865b1d1ee590fd894f665aa2914a
Javier Ale <javierale25@live.com>
2021-07-07 18:58:34 -0300

    Change to database remote

--- api/src/database.js
+++ api/src/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'developer',
-    database: 'todolist',
+    host: 'bszgp8zmp3dgdntvlyaz-mysql.services.clever-cloud.com',
+    user: 'uclkjybhxl8cqxef',
+    password: '9buhhgj6oZrZ2ShsnCoW',
+    database: 'bszgp8zmp3dgdntvlyaz',
     multipleStatements: true
 });
 
